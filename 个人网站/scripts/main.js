4835f39c5db28e0cb0ce3be18443ad79df32cb14
771054284 <38810269+771054284@users.noreply.github.com>
2018-04-30 23:43:13 +0800

    Update main.js

--- 个人网站/scripts/main.js
+++ 个人网站/scripts/main.js
@@ -13,7 +13,7 @@ var myHeading = document.querySelector('h1');
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+  myHeading.innerHTML = myName+'个人网站 ';
 }
 if(!localStorage.getItem('name')) {
   setUserName();
